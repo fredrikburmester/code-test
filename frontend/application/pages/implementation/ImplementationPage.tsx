2d6809be20573c1e01a79a858343be08aa17dc55
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-05-28 17:15:05 +0200

    feat: basic logic first commit

--- frontend/application/pages/implementation/ImplementationPage.tsx
+++ frontend/application/pages/implementation/ImplementationPage.tsx
@@ -1,15 +1,124 @@
 import * as React from "react";
+import { useEffect,useState } from "react";
 
 import * as css from "./ImplementationPage.module.scss";
+import axios from "axios";
 
-class ImplementationPage extends React.Component {
-    public render() {
-        return (
-            <>
-                <h2 className={css.title}>Lägg implementationen här</h2>
-            </>
-        );
-    }
+interface Elevator {
+    id: number,
+    floor: number,
+    direction: string,
+    destination: number,
 }
 
-export default ImplementationPage;
+const API_URL = 'http://localhost:3000/elevators'
+
+const ImplementationPage = () => {
+    const defaultElevators: Elevator[] = [];
+
+    const [requestedFloor, setRequestedFloor] = useState(null);
+    const [elevators, setElevators] = useState(defaultElevators);
+    const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState<boolean>(true);
+    const [error, setError]: [string, (error: string) => void] = useState("");
+
+    const callElevator = async (floor: number) => {
+        // Reseve the closest elevator
+        const new_elevator = await axios.get<Elevator>(`${API_URL}/reserve/${floor}`)
+        const elevator = elevators.find(e => e.id === new_elevator.data.id);
+        if (!elevator) return 
+        setRequestedFloor(floor);
+        elevator.floor = new_elevator.data.floor;
+        elevator.direction = new_elevator.data.direction;
+        elevator.destination = new_elevator.data.destination;
+        setElevators([...elevators]);
+
+        // Move the elevator to the floor
+        axios.post<Elevator>(`${API_URL}/call`, { floor: floor })
+            .then((res) => {
+                console.log("callElevator 2",res.data)
+                
+                // find and update the elevator
+                const elevator = elevators.find(e => e.id === res.data.id);
+                if (!elevator) return 
+
+                elevator.floor = res.data.floor;
+                elevator.direction = res.data.direction;
+                elevator.destination = res.data.destination;
+                setElevators([...elevators]);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    useEffect(() => {
+        axios
+            .get<Elevator[]>(`${API_URL}/status`)
+            .then(response => {
+                setElevators(response.data);
+                setLoading(false);
+            })
+            .catch(ex => {
+                console.log(ex)
+                let error = ex
+                if(ex?.response && ex?.response?.data) {
+                error =
+                    ex.response.status === 404
+                    ? "Resource Not found"
+                    : "An unexpected error has occurred";
+                }
+                setError(error);
+                setLoading(false);
+            });
+        }, []);
+
+    return (
+        <>
+            <h1>Elevators</h1>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {!loading && !error &&
+                <>
+                    <table className={css.table}>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Floor</th>
+                                <th>Direction</th>
+                                <th>Destination</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {elevators.map(elevator =>
+                                <tr key={elevator.id}>
+                                    <td>{elevator.id}</td>
+                                    <td>{elevator.floor}</td>
+                                    <td>{elevator.direction}</td>
+                                    <td>{elevator.destination}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    <div className={css.elevatorDisplays}>
+                        {elevators.map(elevator =>
+                            <div key={elevator.id} className={css.elevatorDisplay}>
+                                {elevator.direction !== 'idle' && <p>{elevator.direction == 'up' ? '^' : 'v'}</p>}
+                                {elevator.direction === 'idle' && <p style={elevator.floor == requestedFloor ? {color: 'green'} : {}}>{elevator.floor}</p>}
+                            </div>
+                        )}
+                    </div>
+                </>
+            }
+            <h1 className={css.title}>Call elevator to floor</h1>
+            <div className={css.elevatorButtons}>
+                {Array.apply(null, Array(20)).map((_: any, floor: any) =>
+                    <button style={floor + 1 === requestedFloor ? {border: '4px solid green'} : {}} key={floor + 1} onClick={() => callElevator(floor + 1)}>{floor + 1}</button>
+                    )}
+            </div>
+            
+        </>
+    );
+}
+
+export default ImplementationPage
+
