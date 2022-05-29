5ffe36b530a8c68064281490e9587a718a2360e3
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-05-29 10:10:51 +0200

    feat: enum for directions, prepare for queue

--- backend/src/Elevator.ts
+++ backend/src/Elevator.ts
@@ -1,8 +1,14 @@
 export interface ElevatorProps {
     id: number;
     floor: number;
-    direction: string;
+    direction: Direction;
     destination: number;
+}
+
+export enum Direction {
+    UP = "up",
+    DOWN = "down",
+    IDLE = "idle"
 }
 
 export class Elevator {
@@ -13,18 +19,22 @@ export class Elevator {
      * @param {number} floor - The current floor of the elevator
      * @param {string} direction - The direction of the elevator (up, down)
      * @param {number} destination - The destination floor of the elevator
+     * @param {number[]} queue - The queue of floors to go to
      */
     id: number
     floor: number
     direction: string
     destination: number
+    queue: number[]
 
     constructor(id: number, floor: number) {
         this.id = id;
         this.floor = floor;
         this.direction = "idle";
         this.destination = null;
+        this.queue = [];
     }
+    
     move(floor: number): Promise<number> {
         /**
          * Moves the elevator to the given floor
