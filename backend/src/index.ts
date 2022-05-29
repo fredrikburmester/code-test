098eecd35b52dda917ae3034221216828b069285
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-05-29 11:21:25 +0200

    feat: implementation with sockets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
-import * as cors from "kcors";
-import * as Koa from "koa";
-import * as bodyparser from "koa-bodyparser";
-import * as Router from "koa-router";
-import { Elevator, Direction, ElevatorProps } from "./elevator";
 
-const app = new Koa();
-const router = new Router();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Elevator, Direction, ElevatorProps } = require('./Elevator');
+
+import cors = require("cors");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors())
+
 const NR_OF_FLOORS = 20;
 
 let e1 = new Elevator(1, 1);
@@ -16,7 +26,7 @@ let e5 = new Elevator(5, 1);
 
 let ELEVATORS = [e1, e2, e3, e4, e5];
 
-export const findClosestElevator = (elevators: Elevator[], floor: number) => {
+export const findClosestElevator = (elevators: typeof Elevator[], floor: number) => {
     /**
      * Finds the closest elevator to the given floor
      *
@@ -38,12 +48,10 @@ export const findClosestElevator = (elevators: Elevator[], floor: number) => {
         return aDelta - bDelta;
     });
 
-    console.log(sortedElevators);
-
     // Pick first not idle elevator
     for(let i = 0; i < sortedElevators.length; i++) {
         const e = sortedElevators[i];
-        if(e.direction !== Direction.IDLE) {
+        if(e.direction === Direction.IDLE) {
             closest = e;
             break;
         }
@@ -56,54 +64,31 @@ export const findClosestElevator = (elevators: Elevator[], floor: number) => {
     return closest;
 }
 
-// Get the status of all elevators
-router.get("/elevators/status", (context) => {
-    context.response.body = ELEVATORS;
-    context.response.status = 200;
+
+app.get('/elevators/status', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(ELEVATORS));
+    res.status = 200;
+    res.send();
 });
 
-// Get the closest elevator to a floor and reserve it to be moved
-router.post("/elevators/reserve", (context) => {
+io.on('connection', (socket: any) => {
+    socket.emit("status", ELEVATORS);
 
-    const floor = parseInt(context.request.body.floor);
-    console.log(context.request.body.floor)
+    socket.on("call", async (floor: number) => {
+        const closestElevator = findClosestElevator(ELEVATORS, floor);
+        closestElevator.queue.push(floor);
 
-    if(floor < 1 || floor > NR_OF_FLOORS) {
-        context.response.body = "Floor out of range";
-        context.response.status = 404;
-        return
-    }
-    
-    const closestElevator = findClosestElevator(ELEVATORS, floor);
+        // This async function is called when the elevator has reached the floor
+        closestElevator.move(socket, ELEVATORS).then(() => {
+            socket.emit("status", ELEVATORS);
+        })
 
-    closestElevator.destination = parseInt(context.params.floor);
-    closestElevator.destination = floor;
-    if (closestElevator.floor < floor) {
-        closestElevator.direction = Direction.UP;
-    } else if (closestElevator.floor > floor) {
-        closestElevator.direction = Direction.DOWN;
-    } else {
-        closestElevator.direction = Direction.IDLE;
-    }
-
-    context.response.body = closestElevator;
-    context.response.status = 202;
-})
-
-// Get the elevator with the closest floor to the requested floor
-router.post("/elevators/call", async (context) => {
-    let floor = parseInt(context.request.body.floor);
-    let closest = findClosestElevator(ELEVATORS, floor);
-
-    await closest.move(floor);
-
-    context.response.body = closest;  
-    context.response.status = 200;
+        // This will be called after the elevator has started moving but before the elevator has reached the floor
+        socket.emit("status", ELEVATORS);
+    });
 });
 
-app.use(bodyparser({
-    enableTypes: ["json"],
-}));
-app.use(cors());
-app.use(router.routes());
-app.listen(3000);
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
